ce79447aae1bd7151d258147ff9e193df3773ce4
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2024-10-01 15:11:34 +0100

    Hardcoded login system for testing

--- Demos/demoNode/nginx/main.js
+++ Demos/demoNode/nginx/main.js
@@ -2,3 +2,14 @@ window.onload = function() {
     alert("Hi!!!");
 }
 
+function login() {
+    const Username = document.getElementById('username').value;
+    const Password = document.getElementById('password').value;
+
+    if (Username == ben & Password == 123){
+        alert("Successfully logged in");
+    } else {
+        alert("Failed to login")
+    }
+
+}
