1da7e4ae84383306cf7b9a4813a0dcbd07f7c185
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2024-10-01 15:41:09 +0100

    Added numbers list to send to server side

--- Demos/demoNode/nginx/main.js
+++ Demos/demoNode/nginx/main.js
@@ -6,10 +6,24 @@ function login() {
     const Username = document.getElementById('username').value;
     const Password = document.getElementById('password').value;
 
-    if (Username == ben & Password == 123){
+    if (Username == "ben" & Password == 123){
         alert("Successfully logged in");
     } else {
         alert("Failed to login")
     }
+
+}
+
+function numbers() {
+    const Numbers = document.getElementById('numbers').value;
+    alert(Numbers)
+}
+
+function getResponse(url, Id, timeout){
+    const req = new XMLHttpRequest();
+    req.open("GET", url, true)
+
+
+
 
 }
