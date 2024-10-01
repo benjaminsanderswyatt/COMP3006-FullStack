8eae7991be87514626ae103bc910a71b5b0b37ee
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2024-10-01 14:36:43 +0100

    Web app using docker and node js

---
+++ Demos/demoNode/app/server.js
@@ -0,0 +1,8 @@
+let http = require("http");
+let port = 9000;
+let server = http.createServer(function(request, response) {
+    response.end("Hello World from Node");
+});
+server.listen(port, function() {
+    console.log("Server listening on port " + port);
+});
