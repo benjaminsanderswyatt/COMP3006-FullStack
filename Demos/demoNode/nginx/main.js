ae6832966f95e1b47f949b39ff44829d10d0469f
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2024-10-01 15:51:25 +0100

    Xml http request to server

--- Demos/demoNode/nginx/main.js
+++ Demos/demoNode/nginx/main.js
@@ -6,7 +6,7 @@ function login() {
     const Username = document.getElementById('username').value;
     const Password = document.getElementById('password').value;
 
-    if (Username == "ben" & Password == 123){
+    if (Username == "ben" && Password == 123){
         alert("Successfully logged in");
     } else {
         alert("Failed to login")
@@ -16,6 +16,9 @@ function login() {
 
 function numbers() {
     const Numbers = document.getElementById('numbers').value;
+
+    getResponse('http://localhost:82/', 1, 5000)
+
     alert(Numbers)
 }
 
@@ -23,7 +26,22 @@ function getResponse(url, Id, timeout){
     const req = new XMLHttpRequest();
     req.open("GET", url, true)
 
+    req.timeout = timeout;
 
+    req.onreadystatechange = function() {
+        if (req.readyState === 4) {
+            if (req.status === 200) {
+                alert("Response: " + req.responseText);
+            } else {
+                alert("Error: " + req.status);
+            }
+        }
+    };
 
+    req.ontimeout = function() {
+        alert("Request timed out after " + timeout + "ms");
+    };
+
+    req.send();
 
 }
