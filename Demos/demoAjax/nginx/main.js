db7f75518fb996ac32f621d38b9ce4fb44d094c7
Ben <benjamin.sanders-wyatt@students.plymouth.ac.uk>
2024-10-08 14:43:17 +0100

    Added destination & platforms to table

--- Demos/demoAjax/nginx/main.js
+++ Demos/demoAjax/nginx/main.js
@@ -1,21 +1,22 @@
 window.onload = function() {
     let request = new XMLHttpRequest();
     request.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status == 200) {
-    let elem = document.getElementById("tabTrains");
-    let trains = JSON.parse(this.responseText);
-    let rows = "";
-    for (i=0; i<trains.trains.length; i++) {
-    let train = trains.trains[i];
-    rows += "<tr>"
-    + "<td>" + train.departs + "</td>"
-    + "</tr>";
-    }
-    elem.innerHTML = rows;
-    }
+        if (this.readyState === 4 && this.status == 200) {
+            let elem = document.getElementById("tabTrains");
+            let trains = JSON.parse(this.responseText);
+            let rows = "";
+            for (i=0; i<trains.trains.length; i++) {
+                let train = trains.trains[i];
+                rows += "<tr>"
+                + "<td>" + train.departs + "</td>"
+                + "<td>" + train.stops[train.stops.length - 1].name + "</td>"
+                + "<td>" + train.platform + "</td>"
+                + "</tr>";
+            }
+            elem.innerHTML = rows;
+        }
     };
     let url = "https://web.socem.plymouth.ac.uk/COMP3006/trains/trains";
     request.open("GET", url, true);
     request.send();
-    }
-    
+}
